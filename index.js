266c3dc73e627d6c7cc5fb86a95a90f27cfdee95
Togi <ThomasGiro@aol.com>
2026-06-22 15:05:35 +0200

    Add teaching class and education

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-mainPanel = $('#jobs');
+jobPanel = $('#jobs');
 
-function generateCard(job) {
+function generateJobCard(job) {
     var $card = $(`
     <div class='card'>
         <div class='row g-0'>
@@ -48,13 +48,68 @@ function generateCard(job) {
         $bulletpoints.append("<li>"+bulletpoint+"</li>");
     }
 
-    mainPanel.append($card);
+    jobPanel.append($card);
 
-    mainPanel.append($("<hr/>"));
+    jobPanel.append($("<hr/>"));
+}
+
+teacherPanel = $('#teacher');
+function generateTeachCard(job) {
+    var $card = $(`
+    <div class='card'>
+        <div class='row g-0'>
+            <div class="col-md-3 text-center">
+                <img id="img" src="https://www.nintendo.com/eu/media/images/assets/nintendo_switch_games/absolum/16x9_Absolum_image1600w.jpg" class="img-fluid rounded-start h-100 object-fit-cover">
+            </div>
+            <div class="col-md-9">
+                <div class="card-body">
+                    <div class="d-flex">
+                        <h4 id="name" class="card-title me-auto">Absolum</h4>
+                        <h6 id="techs"></h6>
+                    </div>
+                    <h5 id="year-company" class="card-subtitle mb-2 text-body-secondary">2025 - Dotemu</h5>
+                    <p class="card-text">
+                        <ul id="bulletpoints">
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>` );
+
+    $card.find('#img').attr("src", job.img);
+
+    $card.find('#name').html(job.name);
+
+    var $techs = $card.find('#techs');
+    for(var tech of job.techs) {
+        var $techBadge =$(`<span class="badge text-bg-secondary">` + tech + `</span>`);
+        
+        $techs.append($techBadge, " ");
+    }
+
+    if(job.company) {
+        $card.find('#year-company').html(job.year + " - " + job.company);
+    } else {
+        $card.find('#year-company').html(job.year);
+    }
+
+    var $bulletpoints = $card.find('#bulletpoints');
+    for(var bulletpoint of job.bulletpoints) {
+        $bulletpoints.append("<li>"+bulletpoint+"</li>");
+    }
+
+    teacherPanel.append($card);
+
+    teacherPanel.append($("<hr/>"));
 }
 
 $.getJSON("portfolio.json", function(json) {
     for(var job of json.jobs) {
-        generateCard(job);
+        generateJobCard(job);
+    }
+    console.log(json);
+    for(var teach of json.teaching) {
+        generateTeachCard(teach);
     }
 });
